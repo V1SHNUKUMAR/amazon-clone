3a9e2c1ace9ab9eff32141d4359a1f7b9e49d380
Vishnu kumar <vk8273577360@gmail.com>
2023-07-12 17:33:58 +0530

    slider completed

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -2,10 +2,26 @@ import React from "react";
 
 import Slider from "./Slider";
 
+import img1 from "../assets/slider/1.jpg";
+import img2 from "../assets/slider/2.jpg";
+import img3 from "../assets/slider/3.jpg";
+import img4 from "../assets/slider/4.jpg";
+import img5 from "../assets/slider/5.jpg";
+import img6 from "../assets/slider/6.jpg";
+
 const Homescreen = () => {
+  const images = [
+    { img: img1, imgUrl: "../assets/slider/1.jpg" },
+    { img: img2, imgUrl: "../assets/slider/2.jpg" },
+    { img: img3, imgUrl: "../assets/slider/3.jpg" },
+    { img: img4, imgUrl: "../assets/slider/4.jpg" },
+    { img: img5, imgUrl: "../assets/slider/5.jpg" },
+    { img: img6, imgUrl: "../assets/slider/6.jpg" },
+  ];
+
   return (
-    <div className="relative">
-      <Slider />
+    <div className="relative max-w-[1500px] mx-auto">
+      <Slider images={images} />
     </div>
   );
 };
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,23 +1,56 @@
 import React from "react";
+// import "react-slideshow-image/dist/styles.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { useRef } from "react";
 
-import img1 from "../assets/slider/1.jpg";
-import img2 from "../assets/slider/2.jpg";
-import img3 from "../assets/slider/3.jpg";
-import img4 from "../assets/slider/4.jpg";
-import img5 from "../assets/slider/5.jpg";
-import img6 from "../assets/slider/6.jpg";
+const Slider = (props) => {
+  const images = props.images;
 
-const Slider = () => {
-  const images = [img1, img2, img3, img4, img5, img6];
+  const swiperRef = useRef();
 
   return (
     <div className="relative w-full">
-      {"hi" &&
-        images.map((image) => {
-          <div className="gradient absolute h-full w-full bg-gradient-to-t from-white to-50% to-transparent bottom-0">
-            <img src={image} alt="image" />
-          </div>;
-        })}
+      <div className="gradient-effect absolute h-[50%] w-full bottom-0 z-10 bg-gradient-to-t from-white"></div>
+      {
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={false}
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper w-full h-full"
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index} className="cursor-pointer">
+              <img src={image.img} alt="sale" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      }
+      <div className="absolute top-[30%] translate-y-[-70%] z-10 w-full text-xl md:top-[23%] md:translate-y-[-78%]md: md:px-6 md:text-4xl">
+        <button
+          className=" absolute left-4  hover:scale-125 active:scale-50 duration-200"
+          onClick={() => swiperRef.current?.slidePrev()}
+        >
+          <i class="fa-solid fa-chevron-right rotate-180"></i>
+        </button>
+        <button
+          className=" absolute right-4 hover:scale-125 active:scale-50 duration-200"
+          onClick={() => swiperRef.current?.slideNext()}
+        >
+          <i class="fa-solid fa-chevron-right"></i>
+        </button>
+      </div>
     </div>
   );
 };
